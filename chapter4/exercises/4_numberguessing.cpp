c09700af5a1888fbd1149b9cb45d2b962235cef3
nola1222 <noahvanboven@gmail.com>
2019-05-01 21:36:30 +0200

    Updating number guesser

--- chapter4/exercises/4_numberguessing.cpp
+++ chapter4/exercises/4_numberguessing.cpp
@@ -7,7 +7,7 @@ int main() {
     cout << "Think of a number between 1 and 100\n"
         << "Is your number you're thinking of equal to, greater than, or less than " << current_guess << "?\n"
         << "Enter 'equals', 'less' or 'greater'.\n";
-    for(string answer = ""; cin >> answer;) {
+    for(string answer; cin >> answer;) {
         if(iteration > 7) {
             cout << "You must be lying because I always guess it within 7 tries ;-). Try again\n";
             break;
@@ -17,7 +17,7 @@ int main() {
             cout << "Yay! I guessed your number, it was: " << current_guess << '\n';
             break;
         } else if(answer == "less") {
-            current_guess /= 2;
+            current_guess = current_guess - 50/pow(2, iteration);
             cout << "Is your number you're thinking of equal to, greater than, or less than " << current_guess << "?\n";
         } else if(answer == "greater") {
             current_guess = current_guess + 50/pow(2, iteration);
