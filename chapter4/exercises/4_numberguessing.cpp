ff08ec2f97285f29369cdf63df8a076984d4cbe6
nola1222 <noahvanboven@gmail.com>
2019-05-01 20:11:49 +0200

    Updating number guessing game

--- chapter4/exercises/4_numberguessing.cpp
+++ chapter4/exercises/4_numberguessing.cpp
@@ -8,6 +8,11 @@ int main() {
         << "Is your number you're thinking of equal to, greater than, or less than " << current_guess << "?\n"
         << "Enter 'equals', 'less' or 'greater'.\n";
     for(string answer = ""; cin >> answer;) {
+        if(iteration > 7) {
+            cout << "You must be lying because I always guess it within 7 tries ;-). Try again\n";
+            break;
+        }
+
         if(answer == "equals") {
             cout << "Yay! I guessed your number, it was: " << current_guess << '\n';
             break;
